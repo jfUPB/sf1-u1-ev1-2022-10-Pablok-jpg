53cc179b68714d2cb47c5b91b93f335f5f9b7088
Pablok-jpg <60201737+Pablok-jpg@users.noreply.github.com>
2022-03-01 23:59:58 -0500

    Final Pablo

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -1,5 +1,4 @@
 #include "SSD1306Wire.h"
-
 #define BOMB_OUT 25 // rojo
 #define LED_COUNT 26 // blanco
 #define UP_BTN 13  //rojo
@@ -9,11 +8,37 @@ int UPbuttonState = 0;
 int lastUPbuttonState = 0;
 int DOWNbuttonState = 0;
 int lastDOWNbuttonState = 0;
+int ARMbuttonState = 0;
+int lastARMbuttonState = 0;
 bool bPress = false;
-
-// Selecciona uno según tu display.
-//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
+bool entro = false;
+char u  = UP_BTN;
+char d  = DOWN_BTN;
+char a  = ARM_BTN;
+static char contrasena[7] = {'u', 'u', 'd', 'd', 'u', 'd', 'a'};
+static char ingreso[7];
+int contador = 0;
 SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
+
+bool digitocorrecto(char *contrasena, char *ingreso)
+{
+  int digito = 0;
+
+  for (int i = 0; i < 7; i++) {    if (contrasena[i] == ingreso[i])
+    {
+      digito = digito + 1;
+    }
+  }
+  if (digito == 7)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
 
 void setup() {
   digitalWrite(BOMB_OUT, LOW);
@@ -39,80 +64,91 @@ void loop() {
   static int y = 20;
   UPbuttonState = digitalRead(UP_BTN);
   DOWNbuttonState = digitalRead(DOWN_BTN);
+  ARMbuttonState = digitalRead(ARM_BTN);
 
-  enum class BombStates {Init, Configuracion, Activa, Desactivada, BOOM};
-  static BombStates bombState =  BombStates::Configuracion;
+  enum class BombStates {Init, Configuracion, Activa, BOOM};
+  static BombStates bombState =  BombStates::Init;
 
   switch (bombState) {
 
     case BombStates::Init: {
+        counter = 20;
+        contador = 0;
         display.init();
         display.setContrast(255);
         display.clear();
         display.setTextAlignment(TEXT_ALIGN_LEFT);
         display.setFont(ArialMT_Plain_16);
-        digitalWrite(BOMB_OUT, LOW);        
+        digitalWrite(BOMB_OUT, LOW);
         display.clear();
-        display.drawString(0, 5, String(counter));
+        display.drawString(x, y, String(counter));
         display.display();
         bombState = BombStates::Configuracion;
-        counter = 20;
+
         break;
       }
 
-    case BombStates::Configuracion:
-
-      if (UPbuttonState != lastUPbuttonState) {
-        if (UPbuttonState == HIGH) {
-          bPress = true;
-          counter++;
-          if (counter > 60) {
-            counter = 60;
-            delay(50);
-          }
-        }
+    case BombStates::Configuracion: {
         display.clear();
         display.setTextAlignment(TEXT_ALIGN_LEFT);
         display.setFont(ArialMT_Plain_16);
-        display.drawString(x, y, String(counter));
-        display.display();
-        lastUPbuttonState = UPbuttonState;
-      }
-
-      else if (DOWNbuttonState != lastDOWNbuttonState) {
-        if (DOWNbuttonState == HIGH) {
-          bPress = true;
-          counter--;
-          if (counter < 10) {
-            counter = 10;
-            delay(50);
-          }
-        }
+        digitalWrite(BOMB_OUT, LOW);
         display.clear();
-        display.setTextAlignment(TEXT_ALIGN_LEFT);
-        display.setFont(ArialMT_Plain_16);
         display.drawString(x, y, String(counter));
         display.display();
+        if (UPbuttonState != lastUPbuttonState) {
+          if (UPbuttonState == HIGH) {
+            bPress = true;
+            counter++;
+            if (counter > 60) {
+              counter = 60;
+              delay(50);
+            }
+          }
+          lastUPbuttonState = UPbuttonState;
+        }
 
-      }
-      lastDOWNbuttonState = DOWNbuttonState;
+        else if (DOWNbuttonState != lastDOWNbuttonState) {
+          if (DOWNbuttonState == HIGH) {
+            bPress = true;
+            counter--;
+            if (counter < 10) {
+              counter = 10;
+              delay(50);
+            }
+          }
+          lastDOWNbuttonState = DOWNbuttonState;
+        }
 
-      digitalWrite(LED_COUNT, HIGH);
-      
-      else if (ARMbuttonState != lastARMbuttonState) {
+        else if (ARMbuttonState != lastARMbuttonState) {
           if (ARMbuttonState == HIGH) {
             bPress = true;
+            contador = 0;
             bombState = BombStates::Activa;
           }
         }
         lastARMbuttonState = ARMbuttonState;
 
         digitalWrite(LED_COUNT, HIGH);
+        break;
+      }
+
+    case BombStates::BOOM: {
+        digitalWrite(LED_COUNT, LOW);
+        digitalWrite(BOMB_OUT, HIGH);
+        display.clear();
+        display.setTextAlignment(TEXT_ALIGN_LEFT);
+        display.setFont(ArialMT_Plain_16);
+        display.drawString(x, y, String("BOOM!!"));
+        display.display();
+        delay (3000);
+        bombState = BombStates::Init;
 
         break;
       }
 
     case BombStates::Activa: {
+        entro = false;
         const uint32_t TiempoLED_COUNT = 500;
         static uint32_t previousTMinus = 0;
         static uint8_t LED_countState = LOW;
@@ -132,26 +168,56 @@ void loop() {
             display.drawString(x, y, String(counter));
             display.display();
           }
-          digitalWrite(LED_COUNT, LED_countState);
         }
+
+        UPbuttonState = digitalRead(UP_BTN);
+        DOWNbuttonState = digitalRead(DOWN_BTN);
+        ARMbuttonState = digitalRead(ARM_BTN);
+        digitalWrite(LED_COUNT, LED_countState);
+        if (UPbuttonState != lastUPbuttonState) {
+          if (UPbuttonState == HIGH) {
+            ingreso[contador] = 'u';
+            Serial.println("up" + contador);
+            contador++;
+          }
+        }
+
+        else if (DOWNbuttonState != lastDOWNbuttonState) {
+          if (DOWNbuttonState == HIGH) {
+            ingreso[contador] = 'd';
+            Serial.println("down" + contador);
+            contador++;
+          }
+        }
+
+        else if (ARMbuttonState != lastARMbuttonState) {
+          if (ARMbuttonState == HIGH) {
+            ingreso[contador] = 'a';
+            Serial.println("arm" + contador);
+            contador++;
+          }
+        }
+        lastUPbuttonState = UPbuttonState;
+        lastARMbuttonState = ARMbuttonState;
+        lastDOWNbuttonState = DOWNbuttonState;
+        if (contador == 7) {
+          entro = true;
+
+          if (digitocorrecto(contrasena, ingreso)) {
+            counter = 20;
+            bombState = BombStates::Configuracion;
+          }
+
+          contador = 0;
+        }
+
         if (counter == 0) {
+          entro = true;
           bombState = BombStates::BOOM;
         }
-        break;
+        if (entro == true) {
+          break;
+        }
       }
-
-    case BombStates::BOOM: {
-        digitalWrite(LED_COUNT, LOW);
-        digitalWrite(BOMB_OUT, HIGH);
-        display.clear();
-        display.setTextAlignment(TEXT_ALIGN_LEFT);
-        display.setFont(ArialMT_Plain_16);
-        display.drawString(x, y, String("BOOM!!"));
-        display.display();
-        delay (3000);
-        bombState = BombStates::Init;
-      }
-
   }
-
 }
